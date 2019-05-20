d2b9eeb03dc799b632b88838bfc32dcabccff5f5
Vagner Barros <vagnerbarrospereira@gmail.com>
2019-05-20 12:00:47 -0300

    .

--- frontend/src/util/config.js
+++ frontend/src/util/config.js
@@ -2,7 +2,7 @@ var URL_ANRON_API
 const TIMEOUT = 30000;
 
 if(process.env.NODE_ENV === 'production'){
-    URL_ANRON_API = 'http://127.0.0.1:5000';
+    URL_ANRON_API = 'http://nodedocker_backend_1:5000';
 }
 else{
     URL_ANRON_API = 'http://127.0.0.1:5000';
